a5f9095fef8fe59b4d27bb8f916183a883728512
Ulan Seitkali <ulan.seitkali@nu.edu.kz>
2022-04-10 15:11:45 +0600

    Initial Commit

---
+++ Task-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task-1
+//
+//  Created by user on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
